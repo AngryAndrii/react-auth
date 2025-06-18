f7cc41f29deebb285ad8d88fac3c73e11838f32e
Andrii Chychkan <mywork1728@gmail>
2025-06-18 23:51:04 +0300

    added useauth hook

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
-import { Box, Button, TextField } from "@mui/material";
-import { useForm } from "react-hook-form";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../firebase/firebase-config";
+import { Box, Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase/firebaseConfig';
 
 function Register() {
   const {
@@ -18,11 +18,11 @@ function Register() {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log("User created:", user);
+        console.log('User created:', user);
         // Here i can save user name in database
       })
       .catch((error) => {
-        console.error("Error:", error.code, error.message);
+        console.error('Error:', error.code, error.message);
       });
   };
 
@@ -30,36 +30,31 @@ function Register() {
     <>
       <Box
         onSubmit={handleSubmit(onSubmit)}
-        component="form"
+        component='form'
         sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-          display: "flex",
-          flexDirection: "column",
+          '& > :not(style)': { m: 1, width: '25ch' },
+          display: 'flex',
+          flexDirection: 'column',
         }}
         noValidate
-        autoComplete="off"
+        autoComplete='off'
       >
+        <TextField {...register('nickname')} label='Nickname' variant='filled' type='text' />
         <TextField
-          {...register("nickname")}
-          label="Nickname"
-          variant="filled"
-          type="text"
+          {...register('email', { required: true })}
+          label='Email'
+          type='mail'
+          variant='filled'
         />
         <TextField
-          {...register("email", { required: true })}
-          label="Email"
-          type="mail"
-          variant="filled"
-        />
-        <TextField
-          {...register("password", { required: true })}
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
+          {...register('password', { required: true })}
+          label='Password'
+          type='password'
+          autoComplete='current-password'
+          variant='filled'
         />
         {errors.exampleRequired && <span>This field is required</span>}
-        <Button variant="outlined" type="submit">
+        <Button variant='outlined' type='submit'>
           Submit
         </Button>
       </Box>
