514faa6fa099c57b4c847ad12af0d2f48ecdd44d
Andrii Chychkan <mywork1728@gmail.com>
2025-06-18 17:21:39 +0300

    added firebase register

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
 function Home() {
+  // const domain = import.meta.env.VITE_AUTH_DOMAIN;
+  // console.log("API DOmain:", domain);
   return <>Home home home</>;
 }
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,45 +1,65 @@
-import { Box, Button, TextField } from '@mui/material';
-import { useForm } from 'react-hook-form';
+import { Box, Button, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebase-config";
 
 function Register() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    // const name = data.nickname;
+    const email = data.email;
+    const password = data.password;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("User created:", user);
+        // Here i can save user name in database
+      })
+      .catch((error) => {
+        console.error("Error:", error.code, error.message);
+      });
+  };
 
   return (
     <>
       <Box
         onSubmit={handleSubmit(onSubmit)}
-        component='form'
+        component="form"
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
-          display: 'flex',
-          flexDirection: 'column',
+          "& > :not(style)": { m: 1, width: "25ch" },
+          display: "flex",
+          flexDirection: "column",
         }}
         noValidate
-        autoComplete='off'
+        autoComplete="off"
       >
-        <TextField {...register('nickname')} label='Nickname' variant='filled' type='text' />
         <TextField
-          {...register('mail', { required: true })}
-          label='Email'
-          type='mail'
-          variant='filled'
+          {...register("nickname")}
+          label="Nickname"
+          variant="filled"
+          type="text"
         />
         <TextField
-          {...register('password', { required: true })}
-          label='Password'
-          type='password'
-          autoComplete='current-password'
-          variant='filled'
+          {...register("email", { required: true })}
+          label="Email"
+          type="mail"
+          variant="filled"
+        />
+        <TextField
+          {...register("password", { required: true })}
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
         />
         {errors.exampleRequired && <span>This field is required</span>}
-        <Button variant='outlined' type='submit'>
+        <Button variant="outlined" type="submit">
           Submit
         </Button>
       </Box>
