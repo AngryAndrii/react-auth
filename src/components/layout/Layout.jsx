b26f678e14b8b4b9b4ae26ac481143fe407d14b1
Andrii Chychkan <mywork1728@gmail.com>
2025-06-23 16:50:56 +0300

    added style for links

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,57 +1,85 @@
-import { Box, List, Button, AppBar } from '@mui/material';
-import { NavLink, Outlet } from 'react-router-dom';
-import { useAuth } from '../../helpers/authContext';
-import useSignOut from '../../hooks/signout';
+import {
+  Box,
+  List,
+  Button,
+  AppBar,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+} from "@mui/material";
+import { NavLink, Outlet } from "react-router-dom";
+import { useAuth } from "../../helpers/authContext";
+import useSignOut from "../../hooks/signout";
+import StyledLayout from "../layout/layout.styled";
 
 function Layout() {
   const { user, loading } = useAuth();
   const handleSignOut = useSignOut();
   if (loading) return <div>Завантаження...</div>;
   return (
-    <Box sx={{ margin: '20px', paddingTop: '50px' }}>
-      <AppBar
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          padding: '5px 25px',
-        }}
-      >
-        <nav>
-          <List sx={{ display: 'flex', flexDirection: 'row', columnGap: '10px' }}>
-            <li>
-              <NavLink to='/'>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to='/register'>Register page</NavLink>
-            </li>
-            <li>
-              <NavLink to='/login'>Login page</NavLink>
-            </li>
-            <li>
-              <NavLink to='/profile'>Profile page</NavLink>
-            </li>
-          </List>
-        </nav>
-        <Box
+    <StyledLayout>
+      <Box sx={{ margin: "20px", paddingTop: "50px" }}>
+        <AppBar
           sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            columnGap: '20px',
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: "5px 25px",
+            backgroundColor: "primary.light",
           }}
         >
-          <Box>{user?.displayName}</Box>
-          <Button variant='contained' onClick={handleSignOut}>
-            Logout
-          </Button>
-        </Box>
-      </AppBar>
-      <main>
-        <Outlet />
-      </main>
-      <footer>{/* <p>&copy; 2025 Angry Andrii</p> */}</footer>
-    </Box>
+          <nav>
+            <List
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                columnGap: "10px",
+                color: "primary.text",
+                textDecoration: "none",
+              }}
+            >
+              <ListItem disablePadding>
+                <NavLink className={"page-link"} to="/">
+                  Home
+                </NavLink>
+              </ListItem>
+              <ListItem disablePadding>
+                <NavLink className={"page-link"} to="/register">
+                  Register page
+                </NavLink>
+              </ListItem>
+              <ListItem disablePadding>
+                <NavLink className={"page-link"} to="/login">
+                  Login page
+                </NavLink>
+              </ListItem>
+              <ListItem disablePadding>
+                <NavLink className={"page-link"} to="/profile">
+                  Profile page
+                </NavLink>
+              </ListItem>
+            </List>
+          </nav>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              columnGap: "20px",
+            }}
+          >
+            <Box>{user?.displayName}</Box>
+            <Button variant="contained" onClick={handleSignOut}>
+              Logout
+            </Button>
+          </Box>
+        </AppBar>
+        <main>
+          <Outlet />
+        </main>
+        <footer>{/* <p>&copy; 2025 Angry Andrii</p> */}</footer>
+      </Box>
+    </StyledLayout>
   );
 }
 
---
+++ src/components/layout/layout.styled.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const StyledLayout = styled.div`
+  .page-link {
+    color: white;
+    text-decoration: none;
+  }
+
+  .page-link:visited {
+    color: white;
+  }
+`;
+
+export default StyledLayout;
