7af6d32958f3ee8e3d8ff9e722be178231370184
Andrii Chychkan <mywork1728@gmail>
2025-06-25 23:39:12 +0300

    fixed validation

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,19 +1,19 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { useForm } from "react-hook-form";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from "../../firebase/firebaseConfig";
-import useRedirect from "../../hooks/useRedirect";
-import { useAuth } from "../../helpers/authContext";
-import { ErrorMessage } from "@hookform/error-message";
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from '../../firebase/firebaseConfig';
+import useRedirect from '../../hooks/useRedirect';
+import { useAuth } from '../../helpers/authContext';
+import { ErrorMessage } from '@hookform/error-message';
 
 function Register() {
   const goTo = useRedirect();
   const { setUser } = useAuth(); // дістаємо setUser з контексту
   const fieldStyle = {
-    backgroundColor: "primary.light",
-    ".MuiFilledInput-root": {
+    backgroundColor: 'primary.light',
+    '.MuiFilledInput-root': {
       input: {
-        color: "black",
+        color: 'black',
       },
     },
   };
@@ -21,7 +21,7 @@ function Register() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({ mode: 'onChange' });
 
   const onSubmit = (data) => {
     const name = data.nickname;
@@ -38,83 +38,75 @@ function Register() {
           .then(() => {
             // Оновлюємо глобальний user у контексті з новим displayName
             setUser({ ...user, displayName: name });
-            console.log("Нікнейм збережено:", name);
+            console.log('Нікнейм збережено:', name);
             // const token = await user.getIdToken();
             // console.log(token);
-            goTo("/profile"); // переходимо на профіль
+            goTo('/profile'); // переходимо на профіль
           })
           .catch((error) => {
-            console.error("Помилка при оновленні профілю:", error.message);
+            console.error('Помилка при оновленні профілю:', error.message);
           });
 
-        console.log("User created:", user);
+        console.log('User created:', user);
       })
       .catch((error) => {
-        console.error("Error:", error.code, error.message);
+        console.error('Error:', error.code, error.message);
       });
   };
 
   return (
-    <Box sx={{ paddingTop: "100px" }}>
+    <Box sx={{ paddingTop: '100px' }}>
       <Box
         onSubmit={handleSubmit(onSubmit)}
-        component="form"
+        component='form'
         sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-          backgroundColor: "secondary.light",
-          margin: "0 auto",
-          width: "fit-content",
-          maxWidth: "100vw",
+          '& > :not(style)': { m: 1, width: '25ch' },
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          backgroundColor: 'secondary.light',
+          margin: '0 auto',
+          width: 'fit-content',
+          maxWidth: '100vw',
         }}
         noValidate
-        autoComplete="off"
+        autoComplete='off'
       >
         <TextField
-          {...register("nickname", {
-            required: "This field is required",
-            maxLength: { value: 20, message: "max lenght is 20 symbols" },
-            minLength: 3,
-            pattern: /^[a-zA-Zа-яА-ЯїЇєЄіІґҐ0-9]+/,
+          {...register('nickname', {
+            required: 'This field is required',
+            maxLength: { value: 20, message: 'max lenght is 20 symbols' },
+            minLength: { value: 3, message: 'min lenght is 3 symbols' },
+            pattern: {
+              value: /^[a-zA-Zа-яА-ЯїЇєЄіІґҐ0-9]+$/,
+              message: 'Only letters and numbers are allowed',
+            },
           })}
-          label="Nickname"
-          variant="filled"
-          type="text"
+          label='Nickname'
+          variant='filled'
+          type='text'
           sx={fieldStyle}
         />
-        <ErrorMessage
-          errors={errors}
-          name="nickname"
-          render={({ messages }) =>
-            messages &&
-            Object.entries(messages).map(([type, message]) => (
-              <p key={type}>{message}</p>
-            ))
-          }
-        />
+        {errors.nickname && (
+          <p style={{ color: 'red', fontSize: '0.9rem' }}>{errors.nickname.message}</p>
+        )}
         <TextField
-          {...register("email", { required: true })}
-          label="Email"
-          type="email"
-          variant="filled"
+          {...register('email', { required: true })}
+          label='Email'
+          type='email'
+          variant='filled'
           sx={fieldStyle}
         />
         <TextField
-          {...register("password", { required: true })}
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
+          {...register('password', { required: true })}
+          label='Password'
+          type='password'
+          autoComplete='current-password'
+          variant='filled'
           sx={fieldStyle}
         />
 
-        <Button
-          variant="contained"
-          type="submit"
-          sx={{ backgroundColor: "primary.main" }}
-        >
+        <Button variant='contained' type='submit' sx={{ backgroundColor: 'primary.main' }}>
           Submit
         </Button>
       </Box>
